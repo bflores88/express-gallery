47e5146a9aca3f55e2f9b73beb6e72b936703b66
kmietotwy <kmietotwy@gmail.com>
2019-05-06 16:10:47 -1000

    added files for authentication, login and register pages

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const fs = require('fs')
-const app = express();
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const fs = require('fs')
 const methodOverride = require('method-override');
+const guard = require('./middleware/guard');
+
 const gallery = require('./routes/gallery.js');
+
+const app = express();
 const PORT = 3000;
 
 app.engine(
